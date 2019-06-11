a11ec699f4036255f1a37cd665d2615775a93617
mrakgope <50702830+mrakgope@users.noreply.github.com>
2019-06-11 15:48:30 +0200

    Add files via upload

---
+++ ft_memmove.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrakgope <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/11 15:33:19 by mrakgope          #+#    #+#             */
+/*   Updated: 2019/06/11 15:47:49 by mrakgope         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char	*s1;
+	unsigned char	*s2;
+	int				i;
+
+	i = 0;
+	s1 = (unsigned char *)dst;
+	s2 = (unsigned char *)src;
+	while (i < len)
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	return (s1);
+}
