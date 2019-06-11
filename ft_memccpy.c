2f110fb66e3415912257aa514718e2f41878ddd3
mrakgope <50702830+mrakgope@users.noreply.github.com>
2019-06-11 16:34:30 +0200

    Add files via upload

---
+++ ft_memccpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrakgope <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/11 12:01:15 by mrakgope          #+#    #+#             */
+/*   Updated: 2019/06/11 16:33:25 by mrakgope         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t len)
+{
+	size_t			i;
+	unsigned char	*s1;
+	unsigned char	*s2;
+
+	i = 0;
+	s1 = (unsigned char *)dst;
+	s2 = (unsigned char *)src;
+	while (i < len && s1 != 0)
+	{
+		s1[i] = s2[i];
+		if (s2[i] == (unsigned char)c)
+			return (s1 + i + 1);
+		i++;
+	}
+	return (s1);
+}
