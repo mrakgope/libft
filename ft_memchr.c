e5688d9891b2126cd660c5efa11d9d926142189e
mrakgope <50702830+mrakgope@users.noreply.github.com>
2019-06-11 15:32:41 +0200

    Add files via upload

---
+++ ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrakgope <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/11 14:13:09 by mrakgope          #+#    #+#             */
+/*   Updated: 2019/06/11 15:30:56 by mrakgope         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *ptr, int c, size_t len)
+{
+	unsigned char	b;
+	unsigned char	*str;
+	size_t			i;
+
+	i = 0;
+	b = (unsigned char)c;
+	str = (unsigned char *)ptr;
+	while (i < len)
+	{
+		if (str[i] == (char)b)
+			return (str + i);
+		i++;
+	}
+	return (0);
+}
